b0074da3876fb80cd11cec8825fe53d7aaf7b0ec
chandra.pramit@prophecy.io <chandra.pramit@prophecy.io>
2026-03-30 08:00:24 +0000

    New     - App        - p245_analysis        - gw_analysis     - Pipeline        - p245        - gw        - 1_challenge_1     - Function/Gem        - generate_schema_name        - prophecy_tmp_source     - Source        - transpiled_sources

---
+++ test_sf_proj/pipelines/p245.py
@@ -0,0 +1,7 @@
+from prophecy_pipeline_sdk.graph import *
+from prophecy_pipeline_sdk.properties import *
+args = PipelineArgs(label = "p245", version = 1)
+
+with Pipeline(args) as pipeline:
+    pass
+
