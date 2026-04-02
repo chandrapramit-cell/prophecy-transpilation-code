2487f1c62307a4741c9a408617da304f7cb495a0
chandra.pramit@prophecy.io <chandra.pramit@prophecy.io>
2026-04-02 10:47:28 +0000

    aaa

---
+++ tb12/pipelines/productos_1/productos_1_merged.py
@@ -0,0 +1,26 @@
+from prophecy_pipeline_sdk.graph import *
+from prophecy_pipeline_sdk.properties import *
+args = PipelineArgs(label = "productos_1_merged", version = 1)
+
+with Pipeline(args) as pipeline:
+    productos_1_part1_pipeline = Process(
+        name = "productos_1_part1_pipeline",
+        properties = PipelineTrigger(
+          maxTriggers = 10000,
+          triggerCondition = "Always",
+          enableMaxTriggers = False,
+          pipelineName = "productos_1_part1",
+          parameters = {}
+        )
+    )
+    productos_1_part2_pipeline = Process(
+        name = "productos_1_part2_pipeline",
+        properties = PipelineTrigger(
+          maxTriggers = 10000,
+          triggerCondition = "Always",
+          enableMaxTriggers = False,
+          pipelineName = "productos_1_part2",
+          parameters = {}
+        )
+    )
+    productos_1_part1_pipeline >> productos_1_part2_pipeline
