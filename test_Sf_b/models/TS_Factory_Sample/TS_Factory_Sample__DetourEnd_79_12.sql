187c5547c841de8de0a3da06ab3aef9c65111fd9
chandra.pramit@prophecy.io <chandra.pramit@prophecy.io>
2026-04-06 07:20:45 +0000

    New     - Function/Gem        - generate_schema_name        - prophecy_tmp_source     - Source        - transpiled_sources     - Pipeline        - TS_Factory_Sample

---
+++ test_Sf_b/models/TS_Factory_Sample/TS_Factory_Sample__DetourEnd_79_12.sql
@@ -0,0 +1,172 @@
+{{
+  config({    
+    "materialized": "ephemeral",
+    "database": "QA_DATABASE",
+    "schema": "PUBLIC"
+  })
+}}
+
+WITH all_samples_yxd_11 AS (
+
+  SELECT *
+  
+  FROM {{ prophecy_tmp_source('TS_Factory_Sample', 'all_samples_yxd_11') }}
+
+),
+
+Detour_67_12_out0 AS (
+
+  SELECT * 
+  
+  FROM all_samples_yxd_11 AS in0
+  
+  WHERE FALSE
+
+),
+
+RecordID_69_12 AS (
+
+  {{
+    prophecy_basics.RecordID(
+      ['Detour_67_12_out0'], 
+      'incremental_id', 
+      'RECORDID', 
+      'integer', 
+      6, 
+      1, 
+      'tableLevel', 
+      'first_column', 
+      [], 
+      []
+    )
+  }}
+
+),
+
+DynamicSelect_70_12 AS (
+
+  {{ prophecy_basics.ToDo('Dynamic Select tool is not supported in sql.') }}
+
+),
+
+DynamicSelect_75_12 AS (
+
+  {{ prophecy_basics.ToDo('Dynamic Select tool is not supported in sql.') }}
+
+),
+
+DynamicSelect_71_12 AS (
+
+  {{ prophecy_basics.ToDo('Dynamic Select tool is not supported in sql.') }}
+
+),
+
+JoinMultiple_76_12 AS (
+
+  SELECT 
+    in0.RECORDID AS RECORDID,
+    in1.RECORDID AS INPUT_HASH2_RECORDID,
+    in2.RECORDID AS INPUT_HASH3_RECORDID
+  
+  FROM DynamicSelect_70_12 AS in0
+  FULL JOIN DynamicSelect_75_12 AS in1
+     ON (in0.RECORDID = in1.RECORDID)
+  FULL JOIN DynamicSelect_71_12 AS in2
+     ON (coalesce(in0.RECORDID, in1.RECORDID) = in2.RECORDID)
+
+),
+
+DynamicSelect_77_12 AS (
+
+  SELECT 
+    UNITS AS UNITS,
+    PRODUCT AS PRODUCT,
+    "NUMBER OF NEW STORES" AS "NUMBER OF NEW STORES",
+    "TOTAL SALES AMOUNT IN DOLLARS" AS "TOTAL SALES AMOUNT IN DOLLARS"
+  
+  FROM JoinMultiple_76_12 AS in0
+
+),
+
+Detour_67_12_out1 AS (
+
+  SELECT * 
+  
+  FROM all_samples_yxd_11 AS in0
+  
+  WHERE TRUE
+
+),
+
+RecordID_74_12 AS (
+
+  {{
+    prophecy_basics.RecordID(
+      ['Detour_67_12_out1'], 
+      'incremental_id', 
+      'RECORDID', 
+      'integer', 
+      6, 
+      1, 
+      'tableLevel', 
+      'first_column', 
+      [], 
+      []
+    )
+  }}
+
+),
+
+DynamicSelect_72_12 AS (
+
+  {{ prophecy_basics.ToDo('Dynamic Select tool is not supported in sql.') }}
+
+),
+
+DynamicSelect_68_12 AS (
+
+  {{ prophecy_basics.ToDo('Dynamic Select tool is not supported in sql.') }}
+
+),
+
+Join_73_12_inner AS (
+
+  SELECT 
+    in1.RECORDID AS RIGHT_RECORDID,
+    in0.*,
+    in1.* EXCLUDE ("RECORDID")
+  
+  FROM DynamicSelect_68_12 AS in0
+  INNER JOIN DynamicSelect_72_12 AS in1
+     ON (in0.RECORDID = in1.RECORDID)
+
+),
+
+DynamicSelect_78_12 AS (
+
+  SELECT 
+    UNITS AS UNITS,
+    PRODUCT AS PRODUCT
+  
+  FROM Join_73_12_inner AS in0
+
+),
+
+DetourEnd_79_12 AS (
+
+  {{
+    prophecy_basics.UnionByName(
+      ['DynamicSelect_77_12', 'DynamicSelect_78_12'], 
+      [
+        '[{"name": "UNITS", "dataType": "Float"}, {"name": "PRODUCT", "dataType": "String"}, {"name": "NUMBER OF NEW STORES", "dataType": "Number"}, {"name": "TOTAL SALES AMOUNT IN DOLLARS", "dataType": "Float"}]', 
+        '[{"name": "UNITS", "dataType": "Float"}, {"name": "PRODUCT", "dataType": "String"}]'
+      ], 
+      'allowMissingColumns'
+    )
+  }}
+
+)
+
+SELECT *
+
+FROM DetourEnd_79_12
