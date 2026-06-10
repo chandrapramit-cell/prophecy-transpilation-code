f075c2ebd20ea7ab7168bddf7c69401b8ee53b17
chandra.pramit@prophecy.io <chandra.pramit@prophecy.io>
2026-06-10 13:34:48 +0000

    Add RM_SEM_V3 SQL models and Databricks external source schemas

---
+++ proj_some/models/RM_SEM_V3/RM_SEM_V3__Join_384_inner.sql
@@ -0,0 +1,39 @@
+{{
+  config({    
+    "materialized": "ephemeral",
+    "database": "sony",
+    "schema": "orch_test"
+  })
+}}
+
+WITH Join_373_inner AS (
+
+  SELECT *
+  
+  FROM {{ ref('RM_SEM_V3__Join_373_inner')}}
+
+),
+
+Database__LOADI_283 AS (
+
+  SELECT *
+  
+  FROM {{ prophecy_tmp_source('RM_SEM_V3', 'Database__LOADI_283') }}
+
+),
+
+Join_384_inner AS (
+
+  SELECT 
+    in0.* EXCEPT (`OD`, `FLT_NBR`, `DPTR_DATE`, `CAP`),
+    in1.* EXCEPT (`LEG_NDOD`, `TT_NDOD`, `DPTR_WEEK`, `SUMREV__LEG_PAX_CNT`)
+  
+  FROM Join_373_inner AS in0
+  INNER JOIN Database__LOADI_283 AS in1
+     ON (in0.NDOD = in1.LEG_NDOD)
+
+)
+
+SELECT *
+
+FROM Join_384_inner
---
+++ proj_some/models/RM_SEM_V3/RM_SEM_V3__googlesheetsoutput_476.sql
@@ -0,0 +1,25 @@
+{{
+  config({    
+    "materialized": "ephemeral",
+    "database": "sony",
+    "schema": "orch_test"
+  })
+}}
+
+WITH AlteryxSelect_338 AS (
+
+  SELECT *
+  
+  FROM {{ ref('RM_SEM_V3__AlteryxSelect_338')}}
+
+),
+
+googlesheetsoutput_476 AS (
+
+  {{ prophecy_basics.ToDo('Component type: google-sheets-output is not supported.') }}
+
+)
+
+SELECT *
+
+FROM googlesheetsoutput_476
