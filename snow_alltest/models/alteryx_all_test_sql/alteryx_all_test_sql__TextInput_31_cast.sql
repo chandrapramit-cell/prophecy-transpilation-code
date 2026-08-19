4263984c413e792e07944d06b42b9653cea5c5f4
chandra.pramit@prophecy.io <chandra.pramit@prophecy.io>
2026-08-19 06:00:07 +0000

    Add snow_alltest dbt project with macros, seeds, and generated modelsgdg

---
+++ snow_alltest/models/alteryx_all_test_sql/alteryx_all_test_sql__TextInput_31_cast.sql
@@ -0,0 +1,30 @@
+{{
+  config({    
+    "materialized": "ephemeral",
+    "database": "QA_DATABASE",
+    "schema": "\"qa_schema\""
+  })
+}}
+
+WITH TextInput_31 AS (
+
+  SELECT * 
+  
+  FROM {{ ref('seed_alteryx_all_test_sql_31')}}
+
+),
+
+TextInput_31_cast AS (
+
+  SELECT 
+    "STORE NUMBER" AS "STORE NUMBER",
+    "UNITS" AS "UNITS",
+    CAST(UNITCOST AS FLOAT) AS UNITCOST
+  
+  FROM TextInput_31 AS in0
+
+)
+
+SELECT *
+
+FROM TextInput_31_cast
